52df545647e9005f095a91e9ccdfb0ad947fabb0
Marlon Sarria <marlonsarria908@gmail.com>
2025-09-05 17:13:43 -0500

    feat: mostrar inmuebles

--- Inmobiliaria/admin.py
+++ Inmobiliaria/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Propietario, Inmueble, Favorito
+
+admin.site.register(Propietario)
+admin.site.register(Inmueble)
+admin.site.register(Favorito)
 
 # Register your models here.
